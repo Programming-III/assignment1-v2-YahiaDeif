1242de55d27f743faf1de83b5a45913fa49b292c
YahiaDeif <yahiadeif23032006@gmail.com>
2025-11-11 09:39:35 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -2,20 +2,22 @@
 #define STUDENT_H
 
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
 
+public:
+    Student(string n = "", int i = 0, int y = 0, string m = "")
+        : Person(n, i), yearLevel(y), major(m) {
+    }
 
-
-
-
-
-
-
-
-
-
+    void display() const override {
+        cout << "Name: " << name << endl;
+        cout << "Year: " << yearLevel << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 #endif
